cc3199efec6d78a1a5f950e33ce69873f3a60eb6
Gourav <73958845+Gaurav062@users.noreply.github.com>
2025-06-07 23:30:49 +0530

    Update Favorites.jsx

--- WatchList/src/pages/Favorites.jsx
+++ WatchList/src/pages/Favorites.jsx
@@ -1,6 +1,6 @@
 import "../css/Favorites.css";
-import { useMovieContext } from "../contexts/MovieContext";
-import MovieCard from "../components/MovieCard";
+import { useMovieContext } from "../contexts/movieContext";
+import MovieCard from "../components/movieCard";
 
 function Favorites() {
   const { favorites } = useMovieContext();
