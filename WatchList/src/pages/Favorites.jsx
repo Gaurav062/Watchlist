fd0e67ad54703e9bb3ffdc3098b6aa671ddcf775
Gaurav062 <gauravduhan062@gmail.com>
2025-06-07 23:02:01 +0530

    Initial commit

---
+++ WatchList/src/pages/Favorites.jsx
@@ -0,0 +1,32 @@
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+  const storedFavs = localStorage.getItem("favorites");
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
