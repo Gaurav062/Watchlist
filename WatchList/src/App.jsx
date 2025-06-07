08ba7a21c2aef1ecb5b02fd4957a96a5ff41e252
Gourav <73958845+Gaurav062@users.noreply.github.com>
2025-06-07 23:07:38 +0530

    Update App.jsx

--- WatchList/src/App.jsx
+++ WatchList/src/App.jsx
@@ -2,7 +2,7 @@ import "./css/App.css";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import { MovieProvider } from "./contexts/MovieContext";
+import { MovieProvider } from "./contexts/movieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
